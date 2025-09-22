370410c164d72ef7761904cd141e7b1d43be3608
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2025-09-22 19:55:42 +0500

    made changes in formatdate function

--- src/app/lib/formatDate.ts
+++ src/app/lib/formatDate.ts
@@ -1,11 +1,11 @@
-
 export function formatDate(dateString: string) {
-    const date = new Date(dateString);
-    return date.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
-    });
-  }
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  return date.toLocaleDateString("en-US", options);
+}
